2ec8940c1996cc6858d2fdf44dc8238a8c29b5e1
Abhinav <abhinavbhardwaj510@gmail.com>
2021-04-29 19:25:58 +0530

    Selective Set Operation

---
+++ selective_set_operation.py
@@ -0,0 +1,27 @@
+def calclen(num1,num2):
+    a = len(num1)
+    b = len(num2)
+    if a!=b :
+        if a>b:
+            for _ in range(a-b):
+                num2='0'+num2
+        else:
+            for _ in range(b-a):
+                num1='0'+num1
+    return num1,num2
+
+def selective_set(A,B):
+    n=len(A)
+    A = list(A)
+    for i in range(n):
+        if B[i]=='1':
+            if A[i]=='0':
+                A[i] = '1'
+
+    A="".join(A)
+    return A
+
+A = input("Enter bits of Register A : ")
+B = input("Enter bits of Register B : ")
+bin_A,bin_B = calclen(A,B)
+print("Selective Set Operation on A =",bin_A,"gives",selective_set(bin_A,bin_B))
